27a06b6aca2cc4de9f069e6ad5e6fcef72d4a3f8
Banjoman93 <crazybanjoman93@gmail.com>
2022-08-08 16:44:22 +0200

    fix code smell

--- tddfirst/src/main/java/com/example/tddfirst/entities/Clinic.java
+++ tddfirst/src/main/java/com/example/tddfirst/entities/Clinic.java
@@ -4,10 +4,8 @@
 package com.example.tddfirst.entities;
 
 import org.springframework.data.annotation.Id;
-//import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
